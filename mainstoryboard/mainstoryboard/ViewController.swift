e8d74edb57df5f3f5aa26aecfdc8ed6b13a4b142
ภาณุวัฒน์ ฟ้ามงคลชัย <panuwatfhamongkolchai@Macintosh.local>
2020-04-19 08:48:44 -0700

    first commit

---
+++ mainstoryboard/mainstoryboard/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  mainstoryboard
+//
+//  Created by ภาณุวัฒน์ ฟ้ามงคลชัย on 19/4/2563 BE.
+//  Copyright © 2563 ภาณุวัฒน์ ฟ้ามงคลชัย. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ mainstoryboard/mainstoryboardTests/mainstoryboardTests.swift
@@ -0,0 +1,34 @@
+//
+//  mainstoryboardTests.swift
+//  mainstoryboardTests
+//
+//  Created by ภาณุวัฒน์ ฟ้ามงคลชัย on 19/4/2563 BE.
+//  Copyright © 2563 ภาณุวัฒน์ ฟ้ามงคลชัย. All rights reserved.
+//
+
+import XCTest
+@testable import mainstoryboard
+
+class mainstoryboardTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
